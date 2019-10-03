ea32ef9f8b49ee16997a0524cf6e9cfafa0024ab
Dan <djchinia@gmail.com>
2019-10-03 15:06:02 -0400

    functional copying for jpg, png, and gif

--- slack_renamer.py
+++ slack_renamer.py
@@ -5,7 +5,6 @@ from shutil import copyfile
   
 # Function to rename multiple files 
 def main():
-
     # make new folder if necessary
     path = os.getcwd()
     dir_name = path + '/new_emojis'
@@ -14,18 +13,27 @@ def main():
         os.makedirs(path + '/new_emojis')
         print ('Directory ', dir_name, ' created')
     except:
-        print ('Directory ', dir_name, ' already exist.')
+        print('Directory ', dir_name, ' already exist.')
+        
 
-    # go through file structure to rename different
+    # go through file structure to rename and reduce the number of folders
+    destination_file = ''
     for filenames in os.walk('./emojis'):
         for file_list in filenames:
             for file_name in file_list:
-                if file_name.endswith(('.png')):
                     root, dirs, files = filenames
                     source_file = root + '/' + files[0]
-                    destination_file = 'new_emojis/' + root[9:] + '.png'
+                    if file_name.endswith(('.png')):
+                        destination_file = 'new_emojis/' + root[9:] + '.png'
+                        copyfile(source_file, destination_file)
+                    elif file_name.endswith(('.jpg')):
+                        destination_file = 'new_emojis/' + root[9:] + '.jpg'
+                        copyfile(source_file, destination_file)
+                    elif file_name.endswith(('.gif')):
+                        destination_file = 'new_emojis/' + root[9:] + '.gif'
+                        copyfile(source_file, destination_file)
                     print source_file, destination_file
-                    copyfile(source_file, destination_file)
+
   
 # driver code and call for main() fn
 if __name__ == '__main__': 
